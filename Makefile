f06adc7f4a79fccc00108104cb468b667637ad06
Ottomata <ottomata@proton.me>
2026-02-23 20:31:04 +0100

    feat: setup sqlc for code gen

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 DATABASE_URL ?= "postgres://postgres:postgres@localhost:5432/db?sslmode=disable&search_path=public"
 MIGRATIONS_PATH ?= backend/migrations
+
 migrate-up:
 	migrate -database $(DATABASE_URL) -path $(MIGRATIONS_PATH) up
 migrate-down:
 	migrate -database $(DATABASE_URL) -path $(MIGRATIONS_PATH) down
 
-.PHONY: migrate-up migrate-down
+sqlc-generate:
+	cd backend && sqlc generate
+
+.PHONY: migrate-up migrate-down sqlc-generate
--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,7 +5,23 @@ import (
 	"acsm/internal/store"
 	"context"
 	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
+
+func init() {
+	m, err := migrate.New(
+		"file://migrations",
+		"postgres://postgres:postgres@localhost:5432/db?sslmode=disable&search_path=public")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	config, err := core.Load()
